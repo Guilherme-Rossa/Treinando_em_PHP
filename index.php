d0ec9939a9594d87b3bd30ff358eceee18964d01
Guilherme-Rossa <rossaguilherme05@gmail.com>
2023-08-21 20:00:23 -0300

    Dados do Servidor

--- index.php
+++ index.php
@@ -1,14 +1,15 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Dados do Servidor</title>
 </head>
-<body>
+<body>  
     <h1>
         <?php 
-            echo "olá, mundo! \u{1F30D}";
+            echo "DADOS DO SERVIDOR \u{1F4BB}";
+            phpinfo();
         ?>
     </h1>
 </body>
